aa91082360c51fa4270dd3cf357c1715a6ef664d
Miles Sandlar <miles.sandlar@gmail.com>
2012-07-21 19:15:53 -0400

    Some more simplifications and clarifications

--- generateStops.rb
+++ generateStops.rb
@@ -41,7 +41,7 @@ def generateStopsFile (userStops, stopTimesCsv)
           day = row[0].match(/^[ABRS]\d+(WKD|SAT|SUN)/)[1]
           hours,minutes,seconds = row[1].split(':')
           seconds = seconds.to_i + (hours.to_i * 3600) + (minutes.to_i * 60)
-        csv << [row[3], stop[1], day, seconds]
+					csv << [row[3], stop[1], day, seconds]
         end
       end
     end
@@ -62,24 +62,29 @@ def stopInfo(stopId, stopLabel)
 end
 
 def promptStops(stopsCsv)
-  allstops = []
-  CSV.parse(stopsCsv) do |row|
-    allstops.push([row[0],row[2]])
-  end
-  addedStops = []
+	allstops = CSV.parse(stopsCsv).collect { |row| [row[0],row[2]] }
 
+	#Continually add stops to added stops until 'done'
+  addedStops = []
   loop do
+
+		#Print Header & Current Stops Added
 		printHeader
 		if (addedStops.length > 0) then
 			puts"Your Subway Stops:".color(:magenta)
 			addedStops.each { |stop| puts "--> #{stopInfo(stop[0], stop[1])}" }
 			print "\n\n"
 		end
-		
+
+		#Prompt User for a stop to add
     print "Add a Subway Stop (ex: York, Prospect, 7 Ave) (or \"done\")\n> "
 		stopname = gets.downcase.chomp!
+
+		#End once user types done
     if (stopname == "done") then
       return addedStops 
+
+
 		else
 			print "\n"
 			options = []
@@ -89,19 +94,17 @@ def promptStops(stopsCsv)
 					options.push(row)
 				end
 			end
-    end
     print "\nEnter desired #{"[stop numbers]".color(:green)}, separated by spaces (ex: 0 2 5)\n> "
     gets.chomp!.split(' ').each do |choice|
 			if (choice.to_i <= options.length) then
 				addedStops.push([options[choice.to_i][0],options[choice.to_i][1]])
 			end
     end
+		end
   end
 end
 
 def main
-	mtaDomain  = $mtaSubwayData.split('/')[0]
-	subwayData = $mtaSubwayData[mtaDomain.length .. $mtaSubwayData.length]
 
 	printHeader
 	$outputFile = ARGV[0] ? ARGV[0] : $outputFile
@@ -121,6 +124,11 @@ def main
 	puts "    (This may take a bit, the ZIP is around 5MB)"
 	puts "    (I hope you gots good internets)"
 
+	#Split up URL for Net:HTTP
+	mtaDomain  = $mtaSubwayData.split('/')[0]
+	subwayData = $mtaSubwayData[mtaDomain.length .. $mtaSubwayData.length]
+
+	#Open Mta site, download zip, and go
 	Net::HTTP.start(mtaDomain) do |http|
 		Zip::Archive.open_buffer(http.get(subwayData).response.body) do |zip|
 
