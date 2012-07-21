82c85c283920de432bc87519d87c1e1fcabf1726
Miles Sandlar <miles.sandlar@gmail.com>
2012-07-21 18:59:47 -0400

    A few simplifications to generateStops

--- generateStops.rb
+++ generateStops.rb
@@ -28,9 +28,11 @@ end
 
 def generateStopsFile (userStops, stopTimesCsv)
 	printHeader
+
   puts "--> Generating Stops CSV".color(:green)
 	puts "    (This may take a little while, go brew some coffee)"
 	puts "    (No worries, this is a one time proccess)"
+
   CSV.open($outputFile, "wb") do |csv|
     csv << ["stop_id", "day", "time"]
     CSV.parse(stopTimesCsv) do |row|
@@ -49,43 +51,44 @@ def generateStopsFile (userStops, stopTimesCsv)
 	puts "--> Stops CSV File Generated to #{$outputFile}, subway.rb may be used now".color(:green)
 end
 
+def getDirection(stopId)
+	return stopId[-1,1] == "N" ? "Northbound" : "Southbound"
+end
+
+def stopInfo(stopId, stopLabel)
+	       info  = "#{getDirection(stopId)}".color(:yellow)
+	       info += " #{stopId.chr} Train ".color(:magenta)
+	return info += "at #{stopLabel} ".color(:red)
+end
+
 def promptStops(stopsCsv)
   allstops = []
   CSV.parse(stopsCsv) do |row|
     allstops.push([row[0],row[2]])
   end
   addedStops = []
+
   loop do
 		printHeader
 		if (addedStops.length > 0) then
 			puts"Your Subway Stops:".color(:magenta)
-			addedStops .each do |stop|
-				print "--> ".color(:yellow)
-
-				print stop[0][-1,1] == "N" ? "Northbound".color(:yellow) : "Southbound".color(:yellow)
-				print " #{stop[0][0]} Train ".color(:magenta)
-				puts "at #{stop[1]} ".color(:red)
-
-
-			end
+			addedStops.each { |stop| puts "--> #{stopInfo(stop[0], stop[1])}" }
 			print "\n\n"
 		end
 		
     print "Add a Subway Stop (ex: York, Prospect, 7 Ave) (or \"done\")\n> "
-    stopname = gets.chomp!
+		stopname = gets.downcase.chomp!
     if (stopname == "done") then
       return addedStops 
-    end
-		print "\n"
-    options = []
-    allstops.each do |row|
-      if (row[0].match(/.+[NS]/) && row[1].downcase.match(/#{stopname.downcase}/)) then
-        print "[#{options.length.to_s}] ".color(:green)
-				print row[0][-1,1] == "N" ? "Northbound".color(:yellow) : "Southbound".color(:yellow)
-				print " #{row[0][0].to_s} Train".color(:magenta)
-				puts " at #{row[1].color(:red)}"
-        options.push(row)
-      end
+		else
+			print "\n"
+			options = []
+			allstops.each do |row|
+				if (row[0].match(/.+[NS]/) && row[1].downcase.match(/#{stopname}/)) then
+					puts "[#{options.length.to_s}] #{stopInfo(row[0], row[1])}".color(:green)
+					options.push(row)
+				end
+			end
     end
     print "\nEnter desired #{"[stop numbers]".color(:green)}, separated by spaces (ex: 0 2 5)\n> "
     gets.chomp!.split(' ').each do |choice|
